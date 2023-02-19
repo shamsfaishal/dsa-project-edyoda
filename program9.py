555aea6a5df3c281b1e2fe4f8d5e023596f72e13
shamsfaishal <75295856+shamsfaishal@users.noreply.github.com>
2023-02-19 12:40:00 +0530

    Add files via upload

---
+++ program9.py
@@ -0,0 +1,35 @@
+#9.Write a program to reverse a stack.
+class Stack: 
+    def __init__(self): 
+        self.items = [] 
+    def isEmpty(self): 
+        return self.items == [] 
+    def push(self,item): 
+        self.items.append(item) 
+        return self.items 
+    def pop(self): 
+        if self.items == []: 
+            return 0 
+        self.items.pop() 
+    def peek(self): 
+        if self.items == []: 
+            return 0 
+        return self.items[len(self.items) - 1] 
+    def size(self): 
+        return len(self.items) 
+ 
+    def getStackItems(self): 
+        return self.items 
+ 
+def reverse(string): 
+    stack = Stack() 
+    ls = [] 
+    newstr = "" 
+    for i in string: 
+        stack.push(i) 
+    ls = stack.getStackItems() 
+    for j in range(len(ls)): 
+        newstr += ls.pop() 
+    print(newstr) 
+ 
+reverse("laddu") 
